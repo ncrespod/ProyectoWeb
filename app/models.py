9d03bb286e269b17670f0597716c589b2da7312c
ncrespod <nicolascrespoakontrol@gmail.com>
2022-02-07 23:27:51 -0500

    cambios de  07/02

--- app/models.py
+++ app/models.py
@@ -7,4 +7,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+class Employee(models.Model):
+    Empid= models.IntegerField()
+    name= models.CharField(max_length=25)
+    address= models.CharField(max_length=100)
+    salary= models.IntegerField()
+    Department= models.CharField(max_length=25)
